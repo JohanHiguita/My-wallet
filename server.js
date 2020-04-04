73a4080e10d9044d82e70ce833f2a44d4c983470
johanhiguita <higuitajohan@gmail.com>
2020-04-04 16:22:41 -0500

    Use Express in the server

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const http = require("http")
 const fs = require("fs")
+const express = require("express")
+const app = express()
+const path = require('path');
 
-//create a server object:
-http.createServer(onRequest).listen(8080) //the server object listens on port 8080
+app.use(express.static("public"))
 
-function onRequest(req, res) {
-	res.writeHead(200, { "Content-Type": "text/html" })
-	//res.write('Hello World!'); //write a response to the client
-	fs.readFile("./index.html", null, function (error, data) {
-		if (error) {
-			res.writeHead(404)
-			res.write("Whoops! File not found!")
-		} else {
-			res.write(data)
-		}
-		res.end()
-	})
-}
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(8080, function () {
+	console.log("Listening on port 8080")
+})
+
